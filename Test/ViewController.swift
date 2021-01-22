1ec352a5c9c29f7936f29c425840c4949f623eee
Fomagran <fomagran6@naver.com>
2021-01-22 18:39:38 +0900

    flexibleSpace를 이용해서 Done버튼 오른쪽에 위치하게 하기

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -19,30 +19,23 @@ class ViewController: UIViewController {
         let toolBar = UIToolbar()
         toolBar.sizeToFit()
         
-        //이전버튼
-        let 이전버튼 = UIButton.init(type: .custom)
-        이전버튼.setTitle("이전", for: .normal)
-        이전버튼.setTitleColor(.white, for: .normal)
-        이전버튼.backgroundColor = .black
-        이전버튼.layer.cornerRadius = 10
-        이전버튼.addTarget(self, action:#selector(doneBtnfromKeyboardClicked), for:.touchUpInside)
-        이전버튼.frame = CGRect.init(x: 0, y: 0, width:50, height: 30)
         
         //다음버튼
         let 다음버튼 = UIButton.init(type: .custom)
-        다음버튼.setTitle("다음", for: .normal)
+        다음버튼.setTitle("Done", for: .normal)
         다음버튼.setTitleColor(.black, for: .normal)
         다음버튼.backgroundColor = .white
         다음버튼.layer.cornerRadius = 10
         다음버튼.addTarget(self, action:#selector(doneBtnfromKeyboardClicked), for:.touchUpInside)
         다음버튼.frame = CGRect.init(x: 0, y: 0, width:50, height: 30)
         
-        //UIBarButtonItem에 UIButton을 넣어줌
-        let 바이전버튼 = UIBarButtonItem.init(customView: 이전버튼)
+        
+        let 빈공간 = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        
         let 바다음버튼 = UIBarButtonItem.init(customView: 다음버튼)
         
         //toolbar 아이템에 바이전버튼,바다음버튼을 넣어준다.
-        toolBar.items = [바이전버튼,바다음버튼]
+        toolBar.items = [빈공간,바다음버튼]
         
         textField.inputAccessoryView = toolBar
         
