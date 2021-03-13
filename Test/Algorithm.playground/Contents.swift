0354f274d29b3dec7984cdc07c34d7d8a9e4343c
Fomagran <fomagran6@naver.com>
2021-03-14 00:29:10 +0900

    위치 포함되는지 확인성공!!

--- Test/Algorithm.playground/Contents.swift
+++ Test/Algorithm.playground/Contents.swift
@@ -1,10 +1,42 @@
 import Foundation
 
-func solution(_ s:String) -> Bool {
+struct User:Decodable,Encodable {
+    let name:String
+    let age:Int
+    let gender:Bool
     
-    if s.count == 4 || s.count == 6 {
-        Int(s) ?? 0
+    init(name:String,age:Int,gender:Bool) {
+        self.name = name
+        self.age = age
+        self.gender = gender
     }
-
-    return false
 }
+
+extension Encodable {
+
+    var dict : [String: Any]? {
+        guard let data = try? JSONEncoder().encode(self) else { return nil }
+        guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] else { return nil }
+        return json
+    }
+}
+
+let user1 = User(name: "foma", age: 26, gender: true)
+let user2 = User(name: "gram", age: 26, gender: true)
+let user3 = User(name: "asdf", age: 26, gender: true)
+let dics = [user1.dict!,user2.dict!,user3.dict!]
+
+
+func toObject<T:Decodable>(objectType:T.Type,dictionary:[[String:Any]]) -> [T]? {
+    
+    guard let json = try? JSONSerialization.data(withJSONObject: dictionary) else { return nil}
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+    guard let decodedCountries = try? decoder.decode([T].self, from: json) else { return nil}
+    
+    return decodedCountries
+}
+
+
+print(toObject(objectType: User.self , dictionary: dics))
+
--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -5,52 +5,152 @@
 //  Created by Fomagran on 2021/01/22.
 //
 
-
 import UIKit
+import MapKit
+import CoreLocation
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,MKMapViewDelegate{
     
-    let userDefaults = UserDefaults.standard
-    var allFoods:[Food]?
+     var currentLocation:CLLocationCoordinate2D!
+    
+    var longitude:Double?
+    var latitude:Double?
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadAllFoods()
-        print(allFoods)
+        
+
+        locationManager.delegate = self;
+        locationManager.distanceFilter = kCLLocationAccuracyNearestTenMeters;
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        
+        requestAuthorization()
+     
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+     
+        setupData()
+
+
     }
     
-    private func addFood() {
-        
-        let 짜장면 = Food(name: "짜장면", price: 6000, isDelicious: true)
-        let 샐러드 = Food(name: "샐러드", price: 4500, isDelicious: false)
-        let 소고기 = Food(name: "소고기", price: 25000, isDelicious: true)
-        
-        
-        allFoods = [짜장면,샐러드,소고기]
-        
-        for food in allFoods!{
-            saveAllFoods(food: food)
+    func setupData() {
+        // 1. check if system can monitor regions
+        if CLLocationManager.isMonitoringAvailable(for: CLCircularRegion.self) {
+     
+            // 2. region data
+            let title = "SK 주유소"
+            let coordinate = CLLocationCoordinate2DMake(37.55950191286448, 126.91666066863014)
+            let regionRadius = 100.0
+            
+            // 3. setup region
+            let region = CLCircularRegion(center: CLLocationCoordinate2D(latitude: coordinate.latitude,
+                                                                         longitude: coordinate.longitude), radius: regionRadius, identifier: title)
+            
+            let test2 = CLLocationCoordinate2D(latitude:latitude!,longitude:longitude!)
+            
+            
+            let point1 = CLLocationCoordinate2D(latitude: 37.558529,
+                                              longitude: 126.917449)
+
+            let point2 = CLLocationCoordinate2D(latitude: 37.558630,
+                                              longitude: 126.918196)
+            let point3 = CLLocationCoordinate2D(latitude: 37.559228,
+                                              longitude: 126.918080)
+            let point4 = CLLocationCoordinate2D(latitude: 37.559340,
+                                              longitude: 126.917441)
+            
+            print(region.contains(test2))
+            print(region.contains(point1))
+            print(region.contains(point2))
+            print(region.contains(point3))
+            print(region.contains(point4))
+            
+           
+            locationManager.startMonitoring(for: region)
+     
+            // 4. setup annotation
+            let restaurantAnnotation = MKPointAnnotation()
+            restaurantAnnotation.coordinate = coordinate;
+            restaurantAnnotation.title = "\(title)";
+            mapView.addAnnotation(restaurantAnnotation)
+     
+            // 5. setup circle
+            let circle = MKCircle(center: coordinate, radius: regionRadius)
+            mapView.addOverlay(circle)
         }
-        
+        else {
+            print("System can't track regions")
+        }
+    }
+     
+    // 6. draw circle
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let circleRenderer = MKCircleRenderer(overlay: overlay)
+        circleRenderer.strokeColor = .red
+        circleRenderer.lineWidth = 1.0
+        return circleRenderer
     }
     
-    private func saveAllFoods(food:Food) {
-        if allFoods != nil {
-            if !allFoods!.contains(food) {
-                allFoods!.append(food)
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        showAlert("enter \(region.identifier)")
+    }
+     
+    // 2. user exit region
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        showAlert("exit \(region.identifier)")
+    }
+    
+    
+    func showAlert(_ str:String){
+        print(str)
+    }
+    
+    private func requestAuthorization() {
+               //정확도를 검사한다.
+               locationManager.desiredAccuracy = kCLLocationAccuracyBest
+               //앱을 사용할때 권한요청
+               locationManager.requestWhenInUseAuthorization()
+               locationManager.delegate = self
+               locationManagerDidChangeAuthorization(locationManager)
+       }
+
+}
+
+extension ViewController:CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        if manager.authorizationStatus == .authorizedWhenInUse {
+            currentLocation = locationManager.location?.coordinate
+            longitude = currentLocation.longitude
+            latitude = currentLocation.latitude
+        }
+    }
+    
+}
+
+
+extension CLLocationCoordinate2D {
+
+    func contained(by vertices: [CLLocationCoordinate2D]) -> Bool {
+        let path = CGMutablePath()
+
+        for vertex in vertices {
+            if path.isEmpty {
+                path.move(to: CGPoint(x: vertex.longitude, y: vertex.latitude))
+            } else {
+                path.addLine(to: CGPoint(x: vertex.longitude, y: vertex.latitude))
             }
-        }else{
-            allFoods = [food]
         }
-        userDefaults.setValue(try? PropertyListEncoder().encode(allFoods!), forKey: "Foods")
-        userDefaults.synchronize()
-    }
-    
-    private func loadAllFoods() {
-        if let data = userDefaults.value(forKey: "Foods") as? Data {
-            allFoods = try? PropertyListDecoder().decode(Array<Food>.self,from: data)
-        }
+
+        let point = CGPoint(x: self.longitude, y: self.latitude)
+        print(point)
+        return path.contains(point)
     }
 }
-  
