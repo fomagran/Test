f42b7a85d62a8ea9178afe4257cf39e5f5456a81
Fomagran <fomagran6@naver.com>
2021-06-29 18:58:07 +0900

    컬렉션뷰 크기 자동으로 조정 및 왼쪽 정렬 테스트

---
+++ Test/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  Test
+//
+//  Created by Fomagran on 2021/06/29.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+}
--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -1,46 +1,67 @@
 import UIKit
-import Photos
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var photo:UIImageView!
+    let letters = ["안녕","안녕하세요","안녕하세요 저는 포마입니다.","안녕하세요 만나서 정말 반갑습니다."]
     
-    var allPhotos:PHFetchResult<PHAsset>? = nil
+    @IBOutlet weak var collection: UICollectionView!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        request()
+        configure()
     }
     
-    func request() {
-        PHPhotoLibrary.requestAuthorization { (status) in
-            if status == .authorized {
-                self.retrieveAsset()
-            }
-        }
-    }
-    
-    func assetToImage(asset: PHAsset) -> UIImage {
-        var image = UIImage()
-        let manager = PHImageManager.default()
-        
-        //이 부분을 제가 빠뜨렸네요 ㅜ
-        let options = PHImageRequestOptions()
-        options.deliveryMode = .opportunistic
-        
-        //위 options를 아래 options 파라미터에 넣어주셔야 해요! (현재 nil로 되어있을거에요!)
-        manager.requestImage(for: asset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFill, options: options, resultHandler: {(result, info)->Void in
-            image = result!
-        })
-        return image
-    }
-    
-    func retrieveAsset() {
-        let fetchOptions = PHFetchOptions()
-        allPhotos = PHAsset.fetchAssets(with: .image, options: fetchOptions)
-        let image = assetToImage(asset: (allPhotos?.object(at: 0))!)
-        DispatchQueue.main.async {
-            self.photo.image = image
-        }
+    func configure() {
+        collection.collectionViewLayout = CollectionViewLeftAlignFlowLayout()
+        if let flowLayout = collection?.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+          }
+        collection.delegate = self
+        collection.dataSource = self
     }
 }
+
+extension ViewController:UICollectionViewDelegate {
+    
+}
+
+extension ViewController:UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return letters.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collection.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+        
+        cell.label.text = letters[indexPath.item]
+        
+        return cell
+    }
+}
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+}
+
+class CollectionViewLeftAlignFlowLayout: UICollectionViewFlowLayout {
+    let cellSpacing: CGFloat = 10
+ 
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        self.minimumLineSpacing = 10.0
+        self.sectionInset = UIEdgeInsets(top: 12.0, left: 16.0, bottom: 0.0, right: 16.0)
+        let attributes = super.layoutAttributesForElements(in: rect)
+ 
+        var leftMargin = sectionInset.left
+        var maxY: CGFloat = -1.0
+        attributes?.forEach { layoutAttribute in
+            if layoutAttribute.frame.origin.y >= maxY {
+                leftMargin = sectionInset.left
+            }
+            layoutAttribute.frame.origin.x = leftMargin
+            leftMargin += layoutAttribute.frame.width + cellSpacing
+            maxY = max(layoutAttribute.frame.maxY, maxY)
+        }
+        return attributes
+    }
+}
+
