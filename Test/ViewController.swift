b9b330990577c9b9e8fad40e83a40b8a282d9478
Fomagran <fomagran6@naver.com>
2021-11-03 17:43:23 +0900

    [add] 이미지 확대/축소 테스트

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -2,66 +2,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let letters = ["안녕","안녕하세요","안녕하세요 저는 포마입니다.","안녕하세요 만나서 정말 반갑습니다."]
-    
-    @IBOutlet weak var collection: UICollectionView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         configure()
     }
     
     func configure() {
-        collection.collectionViewLayout = CollectionViewLeftAlignFlowLayout()
-        if let flowLayout = collection?.collectionViewLayout as? UICollectionViewFlowLayout {
-            flowLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
-          }
-        collection.delegate = self
-        collection.dataSource = self
+        imageView.image = UIImage(named: "스폰지밥.png")
+        scrollView.delegate = self
+        scrollView.zoomScale = 1.0
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 2.0
     }
+
 }
 
-extension ViewController:UICollectionViewDelegate {
-    
-}
-
-extension ViewController:UICollectionViewDataSource {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return letters.count
+extension ViewController:UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collection.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
-        
-        cell.label.text = letters[indexPath.item]
-        
-        return cell
-    }
-}
-
-class CollectionViewCell: UICollectionViewCell {
-    @IBOutlet weak var label: UILabel!
-}
-
-class CollectionViewLeftAlignFlowLayout: UICollectionViewFlowLayout {
-    let cellSpacing: CGFloat = 10
- 
-    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-        self.minimumLineSpacing = 10.0
-        self.sectionInset = UIEdgeInsets(top: 12.0, left: 16.0, bottom: 0.0, right: 16.0)
-        let attributes = super.layoutAttributesForElements(in: rect)
- 
-        var leftMargin = sectionInset.left
-        var maxY: CGFloat = -1.0
-        attributes?.forEach { layoutAttribute in
-            if layoutAttribute.frame.origin.y >= maxY {
-                leftMargin = sectionInset.left
-            }
-            layoutAttribute.frame.origin.x = leftMargin
-            leftMargin += layoutAttribute.frame.width + cellSpacing
-            maxY = max(layoutAttribute.frame.maxY, maxY)
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.zoomScale <= 1.0 {
+            scrollView.zoomScale = 1.0
         }
-        return attributes
+        
+        if scrollView.zoomScale >= 2.0 {
+            scrollView.zoomScale = 2.0
+        }
     }
 }
 
